58e6ff56c1330bb36268a5ade2a8c19894f02250
Axel Navarro <navarroaxel@gmail.com>
2021-11-22 14:54:17 -0300

    fix unit tests

--- src/labeler.spec.ts
+++ src/labeler.spec.ts
@@ -1,116 +1,82 @@
-import {getFileLabel, FileStatus, LabelType} from './labeler';
+import {FileStatus, getFileLabel, LabelType} from './labeler';
 
 describe('getFileLabel', () => {
-  describe('when a main page is added', () => {
-    it('should return new command label', () => {
-      expect(getFileLabel({
-        filename: 'pages/common/cat.md',
-        status: FileStatus.added,
-      })).toBe(LabelType.newCommand);
+  describe('when a main page is changed', () => {
+    describe('and the status is added', () => {
+      it('should return new command label', () => {
+        expect(getFileLabel({
+          filename: 'pages/common/cat.md',
+          status: FileStatus.added,
+        })).toBe(LabelType.newCommand);
+      });
+    });
+
+    describe.each([
+      FileStatus.modified,
+      FileStatus.removed,
+      FileStatus.renamed,
+    ])('and the status is %s', (status: FileStatus) => {
+      it('should return page edit label', () => {
+        expect(getFileLabel({
+          filename: 'pages/common/cat.md',
+          status,
+        })).toBe(LabelType.pageEdit);
+      });
     });
   });
 
-  describe('when a main page is modified', () => {
-    it('should return page edit label', () => {
-      expect(getFileLabel({
-        filename: 'pages/windows/dir.md',
-        status: FileStatus.modified,
-      })).toBe(LabelType.pageEdit);
+  describe('when a translation page is changed', () => {
+    describe.each([
+      FileStatus.added,
+      FileStatus.modified,
+      FileStatus.removed,
+      FileStatus.renamed
+    ])('and the status is %s', (status: FileStatus) => {
+      it('should return translation label', () => {
+        expect(getFileLabel({
+          filename: 'pages.de/common/git.md',
+          status,
+        })).toBe(LabelType.translation);
+      });
     });
   });
 
-  describe('when a main page is removed', () => {
-    it('should return page edit label', () => {
-      expect(getFileLabel({
-        filename: 'pages/osx/du.md',
-        status: FileStatus.removed,
-      })).toBe(LabelType.pageEdit);
+  describe('when a Markdown file is changed', () => {
+    describe.each([
+      FileStatus.added,
+      FileStatus.modified,
+      FileStatus.removed,
+      FileStatus.renamed
+    ])('and the status is %s', (status: FileStatus) => {
+      it('should return documentation label', () => {
+        expect(getFileLabel({
+          filename: 'contributing-guides/maintainers-guide.md',
+          status,
+        })).toBe(LabelType.documentation);
+      });
     });
   });
 
-  describe('when a main page is renamed', () => {
-    it('should return page edit label', () => {
-      expect(getFileLabel({
-        previous_filename: 'pages/linux/ls.md',
-        filename: 'pages/common/ls.md',
-        status: FileStatus.renamed,
-      })).toBe(LabelType.pageEdit);
-    });
-  });
-
-  describe('returning a translation label', () => {
-    it.each([
-      [FileStatus.added],
-      [FileStatus.modified],
-      [FileStatus.removed],
-    ])('should return label when page is %s', (status: FileStatus) => {
-      expect(getFileLabel({
-        filename: 'pages.de/common/git.md',
-        status,
-      })).toBe(LabelType.translation);
-    });
-  });
-
-  describe('when a Markdown file is added', () => {
-    it('should return documentation label', () => {
-      expect(getFileLabel({
-        filename: 'contributing-guides/maintainers-guide.md',
-        status: FileStatus.added,
-      })).toBe(LabelType.documentation);
-    });
-  });
-
-  describe('when a Markdown file is modified', () => {
-    it('should return documentation label', () => {
-      expect(getFileLabel({
-        filename: 'README.md',
-        status: FileStatus.modified,
-      })).toBe(LabelType.documentation);
-    });
-  });
-
-  describe('when a Python file is added', () => {
-    it('should return tooling label', () => {
-      expect(getFileLabel({
-        filename: 'scripts/set-alias-page.py',
-        status: FileStatus.added,
-      })).toBe(LabelType.tooling);
-    });
-  });
-
-  describe('when a Python file is modified', () => {
-    it('should return tooling label', () => {
-      expect(getFileLabel({
-        filename: 'scripts/set-more-info-link.py',
-        status: FileStatus.modified,
-      })).toBe(LabelType.tooling);
-    });
-  });
-
-  describe('when a JavaScript file is modified', () => {
-    it('should return tooling label', () => {
-      expect(getFileLabel({
-        filename: 'scripts/build-index.js',
-        status: FileStatus.modified,
-      })).toBe(LabelType.tooling);
-    });
-  });
-
-  describe('when a Bash file is added', () => {
-    it('should return tooling label', () => {
-      expect(getFileLabel({
-        filename: 'scripts/build.sh',
-        status: FileStatus.added,
-      })).toBe(LabelType.tooling);
-    });
-  });
-
-  describe('when a Bash file is modified', () => {
-    it('should return tooling label', () => {
-      expect(getFileLabel({
-        filename: 'scripts/test.sh',
-        status: FileStatus.modified,
-      })).toBe(LabelType.tooling);
+  describe.each(
+    [
+      ['Bash', 'scripts/test.sh'],
+      ['JavaScript', 'scripts/build-index.js'],
+      ['Python', 'scripts/set-more-info-link.py'],
+      ['Yaml', '.github/workflows/ci.yml']
+    ]
+  )('when a %s file is changed', (_, filename: string) => {
+    describe.each([
+      FileStatus.added,
+      FileStatus.modified,
+      FileStatus.removed,
+      FileStatus.renamed
+    ])('and the status is %s', (status: FileStatus) => {
+      it('should return tooling label', () => {
+        expect(getFileLabel({
+          filename,
+          status,
+        })).toBe(LabelType.tooling);
+      });
     });
   });
 });
